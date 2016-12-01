90281bcce32824872a01dc6bb261ab29995be15d
bh_lay <bh_lay@126.com>
2016-12-01 22:35:27 +0800

    初始化CSS

--- tasks/config/less.js
+++ tasks/config/less.js
@@ -15,17 +15,17 @@
  */
 module.exports = function(grunt) {
 
-  grunt.config.set('less', {
-    dev: {
-      files: [{
-        expand: true,
-        cwd: 'assets/styles/',
-        src: ['importer.less'],
-        dest: '.tmp/public/styles/',
-        ext: '.css'
-      }]
-    }
-  });
+	grunt.config.set('less', {
+        dev: {
+			files: [{
+				expand:true,
+				cwd:'assets/styles/',
+				src:'**/*.less',
+				dest: '.tmp/public/styles/',
+				ext: '.css'
+			}]
+		}
+	});
 
-  grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-less');
 };
