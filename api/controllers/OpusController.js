a3a35bb592c7f380ac066eff67adc9d79385f08c
bh_lay <bh_lay@126.com>
2016-12-05 07:50:25 +0800

    调整全局字体样式，去除错误代码

--- api/controllers/OpusController.js
+++ api/controllers/OpusController.js
@@ -7,11 +7,6 @@
 
 module.exports = {
     detail: function (req, res) {
-        OpusModel.find().exec(function(err, user) {
-            return res.jsonp({
-                list: user
-            });
-        });
         return res.view( 'page/opus-detail', {
             title: '作品页面'
         });
