fc42219f1a31b0f20832df560b746bc0a8955e02
bh_lay <bh_lay@126.com>
2016-12-26 22:12:53 +0800

    增加图像处理库

--- api/controllers/api/OpusController.js
+++ api/controllers/api/OpusController.js
@@ -6,6 +6,8 @@
  */
 var fs = require('fs');
 var path = require('path');
+var gm = require('gm');
+var imageMagick = gm.subClass({ imageMagick: true });
 
 module.exports = {
   list: function (req, res) {
@@ -29,23 +31,27 @@ module.exports = {
   },
   addCover: function (req, res) {
     req.file('cover').upload({
-      // 上限10M ~10MB
-      maxBytes: 10000000,
+      // 上限5M
+      maxBytes: 5 * 1000 * 1000,
     },function (err, uploadedFiles) {
-      // var responseJSON = {
-      //   code: 0
-      //   // url: ''
-      // };
       if (err) {
-        return res.negotiate({
-          code: 201
-        });
+        if (err.code == 'E_EXCEEDS_UPLOAD_LIMIT') {
+          return res.negotiate({
+            code: 201,
+            msg: '图片上限5M'
+          });
+        } else {
+          return res.negotiate({
+            code: 202
+          });
+        }
+
       }
 
       // If no files were uploaded, respond with an error.
       if (uploadedFiles.length === 0){
         return res.badRequest({
-          code: 202,
+          code: 203,
           msg: '没有上传文件'
         });
       }
@@ -54,7 +60,7 @@ module.exports = {
       // 上传文件没有后缀名
       if(!extensionMatch){
         return res.badRequest({
-          code: 203,
+          code: 204,
           msg: '上传文件没有有效的后缀名'
         });
       }
@@ -68,19 +74,108 @@ module.exports = {
       var newFilePath = path.join(sails.config.upload.root, fileRelativePath);
 
       // 移动临时文件至新的目录
-      fs.rename(file.fd, newFilePath, function(){
-        if(err){
-          return res.ok({
-            code: 204,
-            msg: '移动文件失败'
+      imageMagick(file.fd)
+          .resize(150, 150, '!') //加('!')强行把图片缩放成对应尺寸150*150！
+          .autoOrient()
+          .write(newFilePath, function(err){
+            if (err) {
+              return res.ok({
+                code: 205,
+                msg: '处理文件失败'
+              });
+            }
+
+            return res.ok({
+              code: 0,
+              url: sails.config.upload.domain + fileRelativePath
+            });
+          });
+    });
+
+  }
+};
+
+/**
+var fs = require('fs');
+var path = require('path');
+var images = require("images");
+
+module.exports = {
+  list: function (req, res) {
+
+  },
+  add: function (req, res) {
+    var params = req.body || {};
+    OpusModel.create({
+      intro: params.intro,
+      title: params.title,
+      cover: params.cover,
+      createBy: req.session.loginUser.id,
+      url: params.url,
+      githubFullName: params.githubFullName
+    }).exec(function (err, finn){
+      if (err) {
+        return res.forbidden(err);
+      }
+      return res.ok(finn);
+    });
+  },
+  addCover: function (req, res) {
+    req.file('cover').upload({
+      // 上限5M
+      maxBytes: 5 * 1000 * 1000,
+    },function (err, uploadedFiles) {
+      if (err) {
+        if (err.code == 'E_EXCEEDS_UPLOAD_LIMIT') {
+          return res.negotiate({
+            code: 201,
+            msg: '图片上限5M'
+          });
+        } else {
+          return res.negotiate({
+            code: 202
           });
         }
-        return res.ok({
-          code: 0,
-          url: sails.config.upload.domain + fileRelativePath
+
+      }
+
+      // If no files were uploaded, respond with an error.
+      if (uploadedFiles.length === 0){
+        return res.badRequest({
+          code: 203,
+          msg: '没有上传文件'
         });
+      }
+      var file = uploadedFiles[0];
+      var extensionMatch = file.filename.match(/(?:\.)[^\.]+$/);
+      // 上传文件没有后缀名
+      if(!extensionMatch){
+        return res.badRequest({
+          code: 204,
+          msg: '上传文件没有有效的后缀名'
+        });
+      }
+
+      var randomID = (new Date().getTime()*100000 + Math.ceil( Math.random() * 100000 )).toString(36);
+      // 动态生成新文件
+      var newFilename = randomID + extensionMatch[0].toLocaleLowerCase();
+      // 文件相对根目录的路径
+      var fileRelativePath = path.join(sails.config.upload.avatarPath, newFilename);
+      // 文件存储目录
+      var newFilePath = path.join(sails.config.upload.root, fileRelativePath);
+
+      images(file.fd)
+          .size(400)
+          .save(newFilePath, {
+            quality : 80
+          });
+
+      return res.ok({
+        code: 0,
+        url: sails.config.upload.domain + fileRelativePath
       });
     });
 
   }
 };
+**/
